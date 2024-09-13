f19d312509f16d6b164866553461581f1c0f5c9b
Lucas Paulo <104645155+LPDP22@users.noreply.github.com>
2024-09-13 15:45:04 -0300

    Add files via upload

---
+++ Coding II/Sol = True.py
@@ -0,0 +1,12 @@
+Sol = True
+Dinheiro = True
+
+sol = input("sol? (sim/nao): ")
+dinheiro = input("dinheiro? (sim/nao): ")
+
+if ("sol" == 'sim'): 
+    if("dinheiro" == 'sim'):
+        print("praia")
+
+else:
+    print("netflix")
---
+++ Coding II/sol and.py
@@ -0,0 +1,8 @@
+sol = input("sol? (sim/nao): ")
+dinheiro = input("dinheiro? (sim/nao): ")
+ 
+if ("sol" == 'sim' and "dinheiro" == 'sim'):
+    print("praia")
+ 
+else:
+    print("netflix")
