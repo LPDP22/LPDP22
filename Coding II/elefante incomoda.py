6817a378664216eb1b2977f94f02f52b1b591cf8
Lucas Paulo <104645155+LPDP22@users.noreply.github.com>
2024-11-01 15:49:50 -0300

    Add files via upload

---
+++ Coding II/elefante incomoda.py
@@ -0,0 +1,35 @@
+elef = 1
+elef2 = 10
+
+
+while elef != 11:
+    if elef == 1:
+        print(f"{elef} elefante incomoda muita gente")
+    if elef % 2 != 0 and elef != 1:
+        print(f"{elef} elefantes incomodam muita gente")
+    if elef == 2:
+        print(f"{elef} elefantes incomodam, incomodam muito mais\n")
+    if elef == 4:
+        print(f"{elef} elefantes incomodam, incomodam, incomodam, incomodam muito mais\n")
+    if elef == 6:
+        print(f"{elef} elefantes incomodam, incomodam, incomodam, incomodam, incomodam, incomodam muito mais\n")
+    if elef == 8:
+        print(f"{elef} elefantes incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam muito mais\n")
+    if elef == 10:
+        print(f"{elef} elefantes incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam muito mais\n")
+    elef += 1
+    
+while elef2 != 0:
+    if elef2 % 2 == 0:
+        print(f"{elef2} elefantes incomodam muita gente")
+    if elef2 == 9:
+        print(f"{elef2} elefantes incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam muito menos\n")
+    if elef2 == 7:
+        print(f"{elef2} elefantes incomodam, incomodam, incomodam, incomodam, incomodam, incomodam, incomodam muito menos\n")
+    if elef2 == 5:
+        print(f"{elef2} elefantes incomodam, incomodam, incomodam, incomodam, incomodam muito menos\n")
+    if elef2 == 3:
+        print(f"{elef2} elefantes incomodam, incomodam, incomodam muito menos\n")
+    if elef2 == 1:
+        print(f"{elef2} elefante incomoda muito menos\n")
+    elef2 -= 1
---
+++ Coding II/patinhos.py
@@ -0,0 +1,10 @@
+duc = 5
+
+while duc != 0:
+    print(f"{duc} patinhos foram passear\nAlem das montanhas para brincar\nA Mamae gritou Quack Quack Quack Quack")
+    if duc != 1:
+        print(f"Mas só {duc-1} patinhos voltaram de la.\n")
+    if duc-1 == 0:
+        print(f"Mas nenhum patinho voltou de la.\n")
+    duc -= 1
+print("Mas a mamae foi procurar \nAlem das montanhas na beira do mar \nA mamae gritou Quack Quack Quack Quack\nE seus filhotinhos voltaram de la")
