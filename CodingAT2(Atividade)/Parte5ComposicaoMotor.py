9ecd2a31b3adcf667bd9b6bf3ad66b92bcb18035
Lucas Paulo <104645155+LPDP22@users.noreply.github.com>
2024-11-20 15:36:30 -0300

    Update and rename Parte5Composicao.py to Parte5ComposicaoMotor.py

--- CodingAT2(Atividade)/Parte5ComposicaoMotor.py
+++ CodingAT2(Atividade)/Parte5ComposicaoMotor.py
@@ -2,6 +2,7 @@
 class Motor:
     def __init__(self, potencia):
         self.__potencia = potencia
-    
+
+    #Para adquirir a Potência
     def getPotencia (self):
         return self.__potencia
