6d25c937a7f1f8dd63d582e54e03a9254574d832
Lucas Paulo <104645155+LPDP22@users.noreply.github.com>
2024-11-20 15:11:53 -0300

    Update Parte4PolimorfismoTesteVeiculo.py

--- CodingAT2(Atividade)/Parte4PolimorfismoTesteVeiculo.py
+++ CodingAT2(Atividade)/Parte4PolimorfismoTesteVeiculo.py
@@ -3,16 +3,22 @@ from Parte3HerancaMoto import Moto
 from Parte5Composicao import Motor
 
 def teste_veiculo(veiculo):
+    #Chamar 'acelerar' duas vezes
     veiculo.acelerar()
     veiculo.acelerar()
+    #Mostrar a velocidade
     print(f'{veiculo.mostrar_velocidade()} km/h\n')
+    Chamar 'frear' uma vez.
     veiculo.frear()
+    #Mostrar a velocidade
     print(f'{veiculo.mostrar_velocidade()} km/h\n')
 
+#Mostrar os dados do carro
 carro = Carro('Fiat', 'Uno', Motor('73 CV'))
 print(f"Dados(Carro): {carro.getMarca()}, {carro.getModelo()}, {carro.getMotor()}")
 teste_veiculo(carro)
 
+#Mostrar os dados de moto
 moto = Moto('Honda', 'CG202')
 print(f"Dados(Moto): {moto.getMarca()}, {moto.getModelo()}")
 teste_veiculo(moto)
